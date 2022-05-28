1e04c5ba6ecf844f5f74f535040df6dc2ae172a7
Dariusz Uchman <dariusz.uchman@snowsoftware.com>
2022-05-28 09:48:57 +0200

    leetcode: init  two-sum implementation

--- two-sum/two-sum.go
+++ two-sum/two-sum.go
@@ -1,6 +1,30 @@
 package two_sum
 
+import "math"
+
 func twoSum(nums []int, target int) []int {
 
-	return []int{}
+	setint := map[int]int{}
+	output := [2]int{}
+
+	for i := 0; i < len(nums); i++ {
+		candidate := nums[i]
+		complementaryIx := -1
+		toBeComplementary := target - candidate
+		found := false
+
+		if val, ok := setint[toBeComplementary]; ok {
+			complementaryIx = val
+			output[0] = int(math.Min(float64(i), float64(complementaryIx)))
+			output[1] = int(math.Max(float64(i), float64(complementaryIx)))
+			found = true
+		} else {
+			setint[candidate] = i
+		}
+		if found {
+			break
+		}
+	}
+
+	return output[:]
 }
