ffe937d1ade971b2aeca5388a5036d2eac4d1d27
darek <dariusz.uchman@snowsoftware.com>
2022-12-23 11:03:55 +0100

    remove-duplicates-from-sorted-list: infrastructure

---
+++ remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.go
@@ -0,0 +1,13 @@
+package remove_duplicates_from_sorted_list
+
+//Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+
+	return nil
+}
