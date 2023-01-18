29be50d6f4752c7cc1e8e476af5202b777196c85
darek <dariusz.uchman@snowsoftware.com>
2023-01-18 08:01:21 +0100

    balanced-binary-tree - solution

--- easy/balanced-binary-tree/balanced-binary-tree.go
+++ easy/balanced-binary-tree/balanced-binary-tree.go
@@ -1,5 +1,9 @@
 package binary_tree_inorder_traversal
 
+import (
+	"math"
+)
+
 // TreeNode definition for a binary tree node.
 type TreeNode struct {
 	Val   int
@@ -9,5 +13,28 @@ type TreeNode struct {
 
 func isBalanced(root *TreeNode) bool {
 
-	return true
+	if balanced(root) != -1 {
+		return true
+	}
+	return false
+}
+
+func balanced(root *TreeNode) int {
+
+	if root == nil {
+		return 0
+	}
+	leftHeight := balanced(root.Left)
+	if leftHeight == -1 {
+		return -1
+	}
+
+	rightHeight := balanced(root.Right)
+	if rightHeight == -1 {
+		return -1
+	}
+	if math.Abs(float64(rightHeight-leftHeight)) > 1 {
+		return -1
+	}
+	return int(math.Max(float64(leftHeight), float64(rightHeight)) + 1)
 }
