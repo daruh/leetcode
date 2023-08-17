a23995ee81f729e235150006e6c85dfaf939500b
Dariusz Uchman <dariusz.uchman@snowsoftware.com>
2023-08-17 20:44:49 +0200

    reverse-prefix-of-word: simple solution

--- easy/reverse-prefix-of-word/reverse-prefix-of-word.go
+++ easy/reverse-prefix-of-word/reverse-prefix-of-word.go
@@ -1,5 +1,30 @@
 package reverse_prefix_of_word
 
 func reversePrefix(word string, ch byte) string {
-	return ""
+
+	var newStr string
+	j := -1
+
+	for i := 0; i < len(word); i++ {
+		if word[i] == ch {
+			j = i
+			break
+		}
+	}
+
+	if j != -1 {
+		k := j
+		for k >= 0 {
+			newStr = newStr + string(word[k])
+			k--
+		}
+
+		for l := j + 1; l < len(word); l++ {
+			newStr = newStr + string(word[l])
+		}
+	} else {
+		return word
+	}
+
+	return newStr
 }
